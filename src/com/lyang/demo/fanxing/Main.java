18b367f3e7fb200b4baaed7c62beb5c9a346a006
DESKTOP-PB4270U\yanglu <691558202@qq.com>
2016-10-20 01:24:51 +0800

    fanxing demo

--- src/com/lyang/demo/fanxing/Main.java
+++ src/com/lyang/demo/fanxing/Main.java
@@ -8,11 +8,40 @@ import java.util.List;
 public class Main {
 
     public static <T extends Fruit> void print(T t){
+        System.out.println(t.getClass());
+        System.out.println(Class.class);
         t.eat();
+    }
+
+    public static <T> void print2(Class<T> clazz){
+        System.out.println(clazz.getClass());
+    }
+
+    public static <T> T createInstance(Class<T> clazz) throws IllegalAccessException, InstantiationException {
+        return clazz.newInstance();
     }
 
     public static void main(String[] args) {
         print(new Apple());
         print(new Banana());
+        print2(Apple.class);
+
+        try {
+            People people = (People) Class.forName("com.lyang.demo.fanxing.People").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            People people = createInstance(People.class);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/com/lyang/demo/fanxing/Test.java
+++ src/com/lyang/demo/fanxing/Test.java
@@ -1,20 +1,27 @@
 package com.lyang.demo.fanxing;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by yanglu on 2016/10/19.
  */
 public class Test<A> {
-    public List<A> list = new ArrayList<A>();
+    public static void printList(List<Object> list) {
+        for (Object elem : list)
+            System.out.println(elem + " ");
+        System.out.println();
+    }
+    public static void printList2(List<?> list) {
+        for (Object elem: list)
+            System.out.print(elem + " ");
+        System.out.println();
+    }
     public static void main(String[] args) {
-        Test<String> test1 = new Test<String>();
-        test1.list.add("hello");
-        System.out.println(test1.list);
-
-        Test<Integer> test2 = new Test<Integer>();
-        test2.list.add(123);
-        System.out.println(test2.list);
+        List<Integer> li = Arrays.asList(1, 2, 3);
+        List<String>  ls = Arrays.asList("one", "two", "three");
+        printList(li);
+        printList(ls);
     }
 }
