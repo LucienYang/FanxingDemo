cce1e9bcc61ec1d822346fbad8563bc4c983712d
LucienYang <691558202@qq.com>
2017-11-14 23:12:22 +0800

    增加List<?>

--- src/com/lyang/demo/fanxing/TestDifferenceBetweenObjectAndT.java
+++ src/com/lyang/demo/fanxing/TestDifferenceBetweenObjectAndT.java
@@ -9,7 +9,6 @@ import java.util.List;
  */
 public class TestDifferenceBetweenObjectAndT {
     public static void printList1(List<Object> list) {
-        list.add(new Object());
         for (Object elem : list)
             System.out.println(elem + " ");
         System.out.println();
@@ -28,18 +27,22 @@ public class TestDifferenceBetweenObjectAndT {
     }
 
     public static void main(String[] args) {
-        List<Integer> li = Arrays.asList(1, 2, 3);
-        List<String>  ls = Arrays.asList("one", "two", "three");
-
+        List<Integer> test1 = Arrays.asList(1, 2, 3);
+        List<String>  test2 = Arrays.asList("one", "two", "three");
+        List<Object> test3 = Arrays.asList(1, "two", 1.23);
+        List<Fruit> test4 = Arrays.asList(new Apple(), new Banana());
         /*
-        * this can not pass compile , because Integer and String
-        * is not subtypes of Object
+        * 下面这句会编译报错，因为参数不能转化成功
         * */
-        printList1(li);
-        printList1(ls);
-        printList2(li);
-        printList2(ls);
-        printList3(li);
-        printList3(ls);
+        printList1(test4);
+        /**/
+        printList1(test3);
+        printList1(test3);
+        printList2(test1);
+        printList2(test2);
+        printList2(test3);
+        printList3(test1);
+        printList3(test2);
+        printList3(test3);
     }
 }
